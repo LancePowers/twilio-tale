6e4b8388dd061929dbc19862f2b08633b7333193
Lance <Lance@thelpstory.com>
2015-08-28 12:48:56 -0600

    removed api require

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
 var twilio = require('twilio');
-var api = require('../../api-token');
 router.get('/story', function(req,res,next){
   res.render('index',{title: story})
 })
